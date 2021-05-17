e7f782ca60d170c763f5f2350fdfd536501bd033
whoismarviin <vini.rosa.roo@gmail.com>
2021-05-17 15:46:36 -0400

    primeiro-commit

--- src/main/kotlin/com/zup/store/Livro.kt
+++ src/main/kotlin/com/zup/store/Livro.kt
@@ -1,10 +1,13 @@
 package com.zup.store
 
+import io.micronaut.core.annotation.Introspected
 import java.util.*
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
+
+@Introspected
 @Entity
 class Livro(
     val nome: String,
