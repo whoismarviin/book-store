f46cc958158f4b5cdd5fa8a29391da568f46ca22
whoismarviin <vini.rosa.roo@gmail.com>
2021-05-26 10:11:11 -0400

    kotest

--- src/test/kotlin/com/zup/store/BookStoreTest.kt
+++ src/test/kotlin/com/zup/store/BookStoreTest.kt
@@ -1,19 +1,13 @@
 package com.zup.store
+
+import io.kotest.core.spec.style.StringSpec
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
-import javax.inject.Inject
 
 @MicronautTest
-class BookStoreTest {
+class ApplicationTest(private val application: EmbeddedApplication<*>) : StringSpec({
 
-    @Inject
-    lateinit var application: EmbeddedApplication<*>
-
-    @Test
-    fun testItWorks() {
-        Assertions.assertTrue(application.isRunning)
+    "test the server is running" {
+        assert(application.isRunning)
     }
-
-}
+})
--- src/test/kotlin/com/zup/store/livroData/controller/LivroControllerTest.kt
+++ src/test/kotlin/com/zup/store/livroData/controller/LivroControllerTest.kt
@@ -1,47 +1,9 @@
 package com.zup.store.livroData.controller
 
-import com.zup.store.Livro
-import com.zup.store.livroData.database.TakeBookImpl
-import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import java.util.*
-import javax.inject.Inject
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 
-@MicronautTest
-internal class LivroControllerTest {
-    @field:Inject
-    lateinit var service: TakeBookImpl
+class LivroControllerTest : FunSpec({
 
 
-    @Test
-    fun deveriaCadastrarUmLivro() {
-        val livro = service.createBook(
-            Livro(
-                UUID.randomUUID(), "O triste fim ",
-                "119",
-                "99282933",
-                19.9F
-            )
-        )
-
-        assertNotNull(livro.id)
-        assertNotNull(livro.nome)
-        assertNotNull(livro.numero_de_paginas)
-        assertNotNull(livro.isbn)
-        assertNotNull(livro.preco)
-
-    }
-
-
-    @Test
-    fun deveriaBuscarTodosOsLivros() {
-        val livros = service.buscaLivros();
-
-        assertNotNull(livros)
-
-    }
-
-
-}
+})
